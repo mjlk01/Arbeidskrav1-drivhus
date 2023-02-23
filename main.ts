497eb4c3d04611df3e45999bf8275a4d81296384
mjlk01 <125974002+mjlk01@users.noreply.github.com>
2023-02-23 11:39:47 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
 })
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        radio.sendNumber(input.temperature())
+        basic.pause(5000)
+        radio.sendNumber(input.lightLevel())
+    }
+})
+radio.setGroup(99)
